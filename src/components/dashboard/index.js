d8fedbf20fdb92713f5099a1adfc5130fd2cc866
HaiTN12 <HaiTN12@fsoft.com>
2023-03-17 16:55:13 +0700

    [HaiTN] Add data to dashboard

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,46 +1,35 @@
-import { Email, PersonAdd, PointOfSale, Traffic } from "@mui/icons-material";
-import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import {
+  Category,
+  ConfirmationNumber,
+  Person,
+  Store,
+} from "@mui/icons-material";
+import { Box, Typography, useMediaQuery } from "@mui/material";
 import React from "react";
 import StatBox from "./chart/StatBox";
-import OverviewChart from "./chart/OverviewChart";
 import { DataGrid } from "@mui/x-data-grid";
 import BreakdownChart from "./chart/BreakdownChart";
+import { useContext } from "react";
+import { StoreContext } from "store";
+import ChartProfit from "./chart/ChartProfit";
+import { useEffect } from "react";
+import { getTotal } from "api";
+import { useState } from "react";
 
 const Dashboard = () => {
-  const theme = useTheme();
   const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
-  const data =[];
-  const isLoading = true;
-  const columns = [
-    {
-      field: "_id",
-      headerName: "ID",
-      flex: 1,
-    },
-    {
-      field: "userId",
-      headerName: "User ID",
-      flex: 1,
-    },
-    {
-      field: "createdAt",
-      headerName: "CreatedAt",
-      flex: 1,
-    },
-    {
-      field: "products",
-      headerName: "# of Products",
-      flex: 0.5,
-      sortable: false,
-      renderCell: (params) => params.value.length,
-    },
-    {
-      field: "cost",
-      headerName: "Cost",
-      flex: 1,
-      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
-    },
-  ];
+  const [state, dispatch] = useContext(StoreContext);
+  const [total, setTotal] = useState([]);
+
+  useEffect(() => {
+    async function fetchApi() {
+      const data = await getTotal({ token: state.accessToken });
+      setTotal(data);
+    }
+    fetchApi();
+  }, [state.accessToken]);
+
+  console.log(total);
   return (
     <Box m="1.5rem 2.5rem">
       <Box
@@ -56,55 +45,31 @@ const Dashboard = () => {
         {/* ROW 1 */}
         <StatBox
           title="Total Users"
-          value="Jello"
-          increase="+14%"
-          description="Since last month"
-          icon={
-            <Email
-              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
-            />
-          }
+          value={total.totalUser}
+          icon={<Person sx={{ color: "#4e6c50", fontSize: "26px" }} />}
         />
         <StatBox
-          title="Total Offices"
-          value="Hello"
-          increase="+21%"
-          description="Since last month"
-          icon={
-            <PointOfSale
-              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
-            />
-          }
+          title="Total Stores"
+          value={total.totalOffice}
+          icon={<Store sx={{ color: "#4e6c50", fontSize: "26px" }} />}
         />
         <Box
           gridColumn="span 8"
           gridRow="span 2"
-          backgroundColor={theme.palette.background.alt}
+          backgroundColor={"#fff"}
           p="1rem"
           borderRadius="0.55rem"
-        >
-          <OverviewChart view="sales" isDashboard={true} />
-        </Box>
+        ></Box>
         <StatBox
           title="Total Products"
-          value="Helo"
-          increase="+5%"
-          description="Since last month"
-          icon={
-            <PersonAdd
-              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
-            />
-          }
+          value={total.totalProduct}
+          icon={<Category sx={{ color: "#4e6c50", fontSize: "26px" }} />}
         />
         <StatBox
           title="Total Cupons"
-          value="Helo"
-          increase="+43%"
-          description="Since last month"
+          value={total.totalCoupon}
           icon={
-            <Traffic
-              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
-            />
+            <ConfirmationNumber sx={{ color: "#4e6c50", fontSize: "26px" }} />
           }
         />
 
@@ -112,55 +77,24 @@ const Dashboard = () => {
         <Box
           gridColumn="span 8"
           gridRow="span 3"
-          sx={{
-            "& .MuiDataGrid-root": {
-              border: "none",
-              borderRadius: "5rem",
-            },
-            "& .MuiDataGrid-cell": {
-              borderBottom: "none",
-            },
-            "& .MuiDataGrid-columnHeaders": {
-              backgroundColor: theme.palette.background.alt,
-              color: theme.palette.secondary[100],
-              borderBottom: "none",
-            },
-            "& .MuiDataGrid-virtualScroller": {
-              backgroundColor: theme.palette.background.alt,
-            },
-            "& .MuiDataGrid-footerContainer": {
-              backgroundColor: theme.palette.background.alt,
-              color: theme.palette.secondary[100],
-              borderTop: "none",
-            },
-            "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-              color: `${theme.palette.secondary[200]} !important`,
-            },
-          }}
+          backgroundColor={"#fff"}
+          p="1rem"
+          borderRadius="0.55rem"
         >
-          <DataGrid
-            loading={isLoading || !data}
-            getRowId={(row) => row._id}
-            rows={(data && data.transactions) || []}
-            columns={columns}
-          />
+          <ChartProfit />
         </Box>
         <Box
           gridColumn="span 4"
           gridRow="span 3"
-          backgroundColor={theme.palette.background.alt}
+          backgroundColor={"#333"}
           p="1.5rem"
           borderRadius="0.55rem"
         >
-          <Typography variant="h6" sx={{ color: theme.palette.secondary[100] }}>
+          <Typography variant="h6" sx={"#333"}>
             Sales By Category
           </Typography>
           <BreakdownChart isDashboard={true} />
-          <Typography
-            p="0 0.6rem"
-            fontSize="0.8rem"
-            sx={{ color: theme.palette.secondary[200] }}
-          >
+          <Typography p="0 0.6rem" fontSize="0.8rem" sx={"#333"}>
             Breakdown of real states and information via category for revenue
             made for this year and total sales.
           </Typography>
